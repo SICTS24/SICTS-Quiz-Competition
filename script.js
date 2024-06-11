8d4ddcc59f3d73e86aaf039f8e955e1f7bdd4128
SICTS24 <eldersiya1234@gmail.com>
2024-06-12 01:01:35 +0530

    update

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ document.getElementById('apiButton').addEventListener('click', function() {
         for (let key in data) {
           if (data.hasOwnProperty(key)) {
             el.innerHTML = el.innerHTML + `\nSchool: ${key}, Time: ${data[key]}`;
+            //problem
           }
         }
       })
